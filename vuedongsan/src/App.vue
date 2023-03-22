0fea635bf3a803275a4f607693f31cf02328692c
cyccc95 <cyccc95@naver.com>
2023-03-22 22:41:35 +0900

    v-if + 모달창

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <div class="black-bg" v-if="모달창열렸니">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="i in 메뉴들" :key="i">{{ i }}</a>
   </div>
@@ -8,7 +16,7 @@
   </div> -->
   <div>
     <img src="./assets/room0.jpg" class="room-img" />
-    <h4>{{ products[0] }}</h4>
+    <h4 @click="모달창열렸니 = true">{{ products[0] }}</h4>
     <p>50만원</p>
     <!-- v-on:click : @click으로 가능 -->
     <button v-on:click="increase(0)">허위매물신고</button>
@@ -39,6 +47,7 @@ export default {
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       price: [50, 60, 70],
       신고수: [0, 0, 0],
+      모달창열렸니: false,
     };
   },
   // vue에서 함수 만드는 곳
@@ -60,6 +69,14 @@ export default {
   color: #2c3e50;
 }
 
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
 .menu {
   background-color: darkslateblue;
   padding: 15px;
@@ -74,5 +91,20 @@ export default {
 .room-img {
   width: 100%;
   margin-top: 40px;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background-color: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
