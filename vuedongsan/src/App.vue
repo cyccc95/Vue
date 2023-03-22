c3e82ae924c7ead1dcfc59a1c2659e5c6e6d590b
cyccc95 <cyccc95@naver.com>
2023-03-22 23:01:09 +0900

    import / export

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -10,52 +10,29 @@
   <div class="menu">
     <a v-for="i in 메뉴들" :key="i">{{ i }}</a>
   </div>
-  <!-- <div v-for="(a, i) in 3" :key="a">
-    <h4>{{ products[i] }}</h4>
-    <p>{{ price[i] }}만원</p>
-  </div> -->
-  <div>
-    <img src="./assets/room0.jpg" class="room-img" />
-    <h4 @click="모달창열렸니 = true">{{ products[0] }}</h4>
-    <p>50만원</p>
-    <!-- v-on:click : @click으로 가능 -->
-    <button v-on:click="increase(0)">허위매물신고</button>
-    <span>신고수 : {{ 신고수[0] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room1.jpg" class="room-img" />
-    <h4>{{ products[1] }}</h4>
-    <p>60만원</p>
-    <button v-on:click="increase(1)">허위매물신고</button>
-    <span>신고수 : {{ 신고수[1] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.jpg" class="room-img" />
-    <h4>{{ products[2] }}</h4>
-    <p>70만원</p>
-    <button v-on:click="increase(2)">허위매물신고</button>
-    <span>신고수 : {{ 신고수[2] }}</span>
+
+  <div v-for="i in 원룸들" :key="i.id">
+    <img :src="i.image" class="room-img" />
+    <h4 @click="모달창열렸니 = true">{{ i.title }}</h4>
+    <p>{{ i.content }}</p>
+    <p>{{ i.price }}만원</p>
   </div>
 </template>
 
 <script>
+import data from "./assets/oneroom";
+
 export default {
   name: "App",
   data() {
     return {
+      원룸들: data,
       메뉴들: ["Home", "Shop", "About"],
-      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
-      price: [50, 60, 70],
-      신고수: [0, 0, 0],
       모달창열렸니: false,
     };
   },
   // vue에서 함수 만드는 곳
-  methods: {
-    increase(i) {
-      this.신고수[i]++;
-    },
-  },
+  methods: {},
   components: {},
 };
 </script>
