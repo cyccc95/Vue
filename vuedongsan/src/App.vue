5e5fb996595a5752ced88791eb4681534ea3b221
cyccc95 <cyccc95@naver.com>
2023-03-22 21:37:24 +0900

    데이터 바인딩

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <div class="red" :style="스타일">{{ products[0] }}</div>
+    <p>{{ price1 }}</p>
+  </div>
+  <div>
+    <div>{{ products[1] }}</div>
+    <p>{{ price2 }}</p>
+  </div>
+  <div>
+    <div>{{ products[2] }}</div>
+    <p>{{ price3 }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      price1: 50,
+      price2: 60,
+      price3: 70,
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      스타일: "color : blue",
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
