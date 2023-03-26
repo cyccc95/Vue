198470bf3940612afb3283bf4e2cfe9f706961f8
cyccc95 <cyccc95@naver.com>
2023-03-26 21:27:22 +0900

    blog 프로젝트 생성

---
+++ blog/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <NavbarComponent></NavbarComponent>
+    <v-main>
+      <ListComponent></ListComponent>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import NavbarComponent from "./components/NavbarComponent.vue";
+import ListComponent from "./components/ListComponent.vue";
+
+export default {
+  name: "App",
+
+  components: { NavbarComponent, ListComponent },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ blog/src/components/NavbarComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar>
+    <v-toolbar-title>My App</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn>Home</v-btn>
+    <v-btn>About</v-btn>
+    <v-btn>Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+};
+</script>
+
+<style></style>
