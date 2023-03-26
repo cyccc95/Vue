085ad197e834a759be57d10bb78edeccd01c45b3
cyccc95 <cyccc95@naver.com>
2023-03-26 20:06:55 +0900

    Transition 태그

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <ModalComponent
-    @closeModal="모달창열렸니 = false"
-    :원룸들="원룸들"
-    :누른번호="누른번호"
-    :모달창열렸니="모달창열렸니"
-  />
+  <Transition name="fade">
+    <ModalComponent
+      @closeModal="모달창열렸니 = false"
+      :원룸들="원룸들"
+      :누른번호="누른번호"
+      :모달창열렸니="모달창열렸니"
+    />
+  </Transition>
 
   <div class="menu">
     <a v-for="i in 메뉴들" :key="i">{{ i }}</a>
@@ -71,5 +73,20 @@ div {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+/* 퇴장 애니메이션은 enter 대신 leave */
+.fade-enter-from {
+  /* opacity: 0; */
+  transform: translateY(-1000px);
+}
+
+.fade-enter-active {
+  transition: all 1s;
+}
+
+.fade-enter-to {
+  /* opacity: 1; */
+  transform: translateY(0px);
 }
 </style>
