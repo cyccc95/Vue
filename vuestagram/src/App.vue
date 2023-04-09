3263f98ce046ba3ed2b35b02bbd41b1aca7dad1b
cyccc95 <cyccc95@naver.com>
2023-04-09 22:10:09 +0900

    탭 기능 테스트

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -18,6 +18,14 @@
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+
+  <div v-if="step == 0">내용0</div>
+  <div v-if="step == 1">내용1</div>
+  <div v-if="step == 2">내용2</div>
+  <button @click="step = 0">버튼0</button>
+  <button @click="step = 1">버튼1</button>
+  <button @click="step = 2">버튼2</button>
+  <div style="margin-top: 500px"></div>
 </template>
 
 <script>
@@ -32,6 +40,7 @@ export default {
     return {
       게시물: postdata,
       더보기: 0,
+      step: 0,
     };
   },
   methods: {
