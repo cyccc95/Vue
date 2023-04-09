0511fab89c0a99ded4a40e54f59f48a600296583
cyccc95 <cyccc95@naver.com>
2023-04-09 20:30:53 +0900

    프로젝트 생성, 레이아웃

---
+++ vuestagram/src/components/ContainerComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <PostComponent />
+    <PostComponent />
+    <PostComponent />
+  </div>
+</template>
+<script>
+import PostComponent from "./PostComponent";
+export default {
+  name: "ContainerComponent",
+  components: { PostComponent },
+};
+</script>
+<style></style>
